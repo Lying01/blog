485e0a59c7503e9f2c828296cf3c7d338aad2188
Lying01 <Lying592575845@gmail.com>
2018-04-02 11:00:43 +0800

    初始化页头、侧栏导航

--- src/components/bl-header.vue
+++ src/components/bl-header.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>Lying的个人博客</p>
+  <div class="head">
+    <img src='../assets/cnodejs.svg' title='cnodejs.svg'>
+  </div>
 </template>
 
 <script>
@@ -10,5 +12,21 @@ export default{
   }
 }
 </script>
-<style>
+<style lang="scss">
+  $BgColor : linear-gradient(#26292C, #2b2b2b);
+  $IconW : 70px;
+  $IconH : 50px;
+  $HeadHeight : 50px;
+  $IconPd : 20px;
+
+  .head {
+    padding-left: $IconPd;
+    height: $HeadHeight;
+    background: $BgColor;
+    img {
+      width: $IconW;
+      height: $IconH;
+    }
+  }
+
 </style>
