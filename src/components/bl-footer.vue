d5279607875d9f404744a3091ffcb883cae76657
Lying01 <Lying592575845@gmail.com>
2018-04-02 11:12:48 +0800

    初始化文章列表、页脚

---
+++ src/components/bl-footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>这是页脚</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/bl-list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    文章列表
+
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+
+</style>
