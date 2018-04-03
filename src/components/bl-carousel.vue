f1ca6d27e8633b04fc24220d2571885eb29b7aef
Lying01 <Lying592575845@gmail.com>
2018-04-03 16:13:25 +0800

    修改轮播图样式，增加文章标题显示

--- src/components/bl-carousel.vue
+++ src/components/bl-carousel.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="block">
-    <div class="el-carousel__summary">
-      <p>fawf</p>
-    </div>
+    <!--<div class="el-carousel__summary">-->
+      <!--<p>fawf</p>-->
+    <!--</div>-->
     <el-carousel height="350px">
       <el-carousel-item v-for="item in 4" :key="item">
-        <h3>{{ item }}</h3>
+        <div class="el-carousel__summary">
+          <p>《文章标题》</p>
+        </div>
       </el-carousel-item>
     </el-carousel>
   </div>
@@ -18,10 +20,45 @@
     }
   }
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
+  $carousel_photo1 : url("../assets/3.jpg") no-repeat center;
+  $carousel_photo2 : url("../assets/5.jpg") no-repeat center;
+
   .el-carousel__item:nth-child(2n) {
-    background: url("../assets/3.jpg") no-repeat center;
+    background : $carousel_photo1 {
+      size: auto 100%;
+    }
+    -moz-background-size:100% 100%;
+  }
+
+  .el-carousel__item:nth-child(2n-1) {
+    background: $carousel_photo2;
     background-size:auto 100%;
     -moz-background-size:100% 100%;
   }
+
+  .el-carousel__summary {
+    width: 100%;
+    background-color: rgba(0,0,0,0.4);
+    opacity: 0.4;
+    padding:10px;
+  }
+
+  .el-carousel__summary p{
+    color: #FFF;
+    text-align: center;
+  }
+
+  @media \0screen\,screen\9 {/* 只支持IE6、7、8 */
+    .el-carousel__summary{
+      background-color:#000000;
+      filter:Alpha(opacity=50);
+      position:static; /* IE6、7、8只能设置position:static(默认属性) ，否则会导致子元素继承Alpha值 */
+      *zoom:1; /* 激活IE6、7的haslayout属性，让它读懂Alpha */
+    }
+    .el-carousel__summary p{
+      position: relative;/* 设置子元素为相对定位，可让子元素不继承Alpha值 */
+    }
+  }
+
 </style>
