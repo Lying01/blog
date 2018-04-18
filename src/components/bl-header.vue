9c1e4d58c533ebb14904408017dc3b698f54b0f5
Lying01 <Lying592575845@gmail.com>
2018-04-18 21:24:18 +0800

    头部添加Logo/添加搜索框/响应式

--- src/components/bl-header.vue
+++ src/components/bl-header.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="head">
-    <img src='../assets/cnodejs.svg' title='cnodejs.svg'>
+    <img src='../assets/logo.png' title='广科IT交流社区'>
+    <el-autocomplete popper-class="my-autocomplete" custom-item="my-remote" v-model="state" :fetch-suggestions="querySearch" placeholder="搜索主题、帖子、用户或分类" icon="close" :on-icon-click="handleIconClick">
+    </el-autocomplete>
   </div>
 </template>
 
@@ -13,20 +15,70 @@ export default{
 }
 </script>
 <style lang="scss">
-  $BgColor : linear-gradient(#26292C, #2b2b2b);
-  $IconW : 70px;
-  $IconH : 50px;
-  $HeadHeight : 50px;
-  $IconPd : 20px;
+  .my-autocomplete {
+    li {
+      line-height: normal !important;
+      padding: 7px !important;
 
-  .head {
-    padding-left: $IconPd;
-    height: $HeadHeight;
-    background: $BgColor;
-    img {
-      width: $IconW;
-      height: $IconH;
+      .name {
+        text-overflow: ellipsis;
+        overflow: hidden;
+      }
+      .addr {
+        font-size: 12px;
+        color: #b4b4b4;
+      }
+
+      .highlighted .addr {
+        color: #ddd;
+      }
+    }
+    .ems {
+      font-size: 12px;
+      color: #b4b4b4;
     }
   }
 
+  $IconW : 40px;
+  $IconH : 40px;
+  $HeadHeight : 50px;
+  $IconPd : 35px;
+
+  .head {
+    display: inline-block;
+    padding-left: $IconPd;
+    height: $HeadHeight;
+    background: #333333;
+    img {
+      margin-top: 5px;
+      width: $IconW;
+      height: $IconH;
+    }
+    input{
+      display: inline-block;
+      margin: 0;
+      padding: 0 20px;
+      line-height: 50px;
+      width: 250px;
+    }
+  }
+
+  .el-autocomplete{
+    vertical-align: top;
+    margin-top: 5px;
+    margin-left: 10%;
+  }
+  @media screen and (max-width: 450px) {
+    .head{
+      padding-left: 15px;
+    }
+  }
+  @media screen and (max-width: 415px) {
+    .el-autocomplete{
+      margin-left: 0;
+    }
+    .head{
+      padding-left: 5px;
+    }
+  }
 </style>
